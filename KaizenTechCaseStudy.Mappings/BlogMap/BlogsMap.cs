86f29dded03ff784b2e133d46c8f2cd79c028938
ibrahim BAVLI <31312276+ibavli@users.noreply.github.com>
2021-03-09 23:27:02 +0300

    Blog map was created

---
+++ KaizenTechCaseStudy.Mappings/BlogMap/BlogsMap.cs
@@ -0,0 +1,17 @@
+﻿using KaizenTechCaseStudy.Entities.BlogEntities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace KaizenTechCaseStudy.Mappings.BlogMap
+{
+    public class BlogsMap : BaseEntityMap<Blogs>
+    {
+        public override void Configure(EntityTypeBuilder<Blogs> entityTypeBuilder)
+        {
+            base.Configure(entityTypeBuilder);
+            entityTypeBuilder.Property(e => e.BlogTitle).IsRequired();
+            entityTypeBuilder.Property(e => e.BlogDescription).IsRequired();
+            entityTypeBuilder.ToTable(typeof(Blogs).Name);
+        }
+    }
+}

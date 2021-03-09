62a82a40cbeec193b6b714a2c4a99d018c9770c6
ibrahim BAVLI <31312276+ibavli@users.noreply.github.com>
2021-03-09 23:18:47 +0300

    blog entity was created

---
+++ KaizenTechCaseStudy.Entities/BlogEntities/Blogs.cs
@@ -0,0 +1,10 @@
+﻿
+namespace KaizenTechCaseStudy.Entities.BlogEntities
+{
+    public class Blogs : BaseEntity
+    {
+        public string BlogTitle { get; set; }
+
+        public string BlogDescription { get; set; }
+    }
+}
